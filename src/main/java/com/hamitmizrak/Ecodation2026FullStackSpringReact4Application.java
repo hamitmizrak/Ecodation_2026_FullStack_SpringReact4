6a9ccbb57c3570a672cb264afe95b711e591227d
HamitMizrak <hamitmizrak@gmail.com>
2026-03-28 11:14:53 +0300

    Spring Security exclude

--- src/main/java/com/hamitmizrak/Ecodation2026FullStackSpringReact4Application.java
+++ src/main/java/com/hamitmizrak/Ecodation2026FullStackSpringReact4Application.java
@@ -2,6 +2,7 @@ package com.hamitmizrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -33,7 +34,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(exclude = {
         // Spring Security Dahil etme
         //SecurityAutoConfiguration.class,
-        //SecurityAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class,
 
         // JWT
