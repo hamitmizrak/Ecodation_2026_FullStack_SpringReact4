7d47b88629c0e055df07dec00b399a97095f24fc
HamitMizrak <hamitmizrak@gmail.com>
2026-03-29 10:12:03 +0300

    - ValidationMessages - DTO - ENTITY - REPOSITORY

---
+++ src/main/java/com/hamitmizrak/data/repository/IBlogCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak.data.repository;
+
+import com.hamitmizrak.data.entity.BlogCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IBlogCategoryRepository extends JpaRepository<BlogCategoryEntity,Long> {
+
+    // Delivered Query
+    Optional<BlogCategoryEntity>  findByCategoryNameIgnoreCase(String categoryName);
+    boolean existsByCategoryNameIgnoreCase(String categoryName);
+}
---
+++ src/main/java/com/hamitmizrak/data/repository/IBlogRepository.java
@@ -0,0 +1,11 @@
+package com.hamitmizrak.data.repository;
+
+import com.hamitmizrak.data.entity.BlogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBlogRepository extends JpaRepository<BlogEntity,Long> {
+
+    // Delivered Query
+}
