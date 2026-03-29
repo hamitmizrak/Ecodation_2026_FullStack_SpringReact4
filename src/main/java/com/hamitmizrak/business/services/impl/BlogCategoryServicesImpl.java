1ecf45b490cda10d13c85c711fa6086eefc8911e
HamitMizrak <hamitmizrak@gmail.com>
2026-03-29 11:39:30 +0300

    - ValidationMessages - DTO - ENTITY - REPOSITORY - MAPPER - Interfaces(Modelmapper,crud,sorting) - Service   * Create   * List   * Find

---
+++ src/main/java/com/hamitmizrak/business/services/impl/BlogCategoryServicesImpl.java
@@ -0,0 +1,117 @@
+package com.hamitmizrak.business.services.impl;
+
+import com.hamitmizrak.bean.ModelMapperBean;
+import com.hamitmizrak.business.dto.BlogCategoryDto;
+import com.hamitmizrak.business.services.interfaces.IBlogCategoryServices;
+import com.hamitmizrak.data.entity.BlogCategoryEntity;
+import com.hamitmizrak.data.mapper.BlogCategoryMapper;
+import com.hamitmizrak.data.repository.IBlogCategoryRepository;
+import com.hamitmizrak.exception.HamitMizrakException;
+import com.hamitmizrak.exception._404_NotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// LOMBOK
+@RequiredArgsConstructor //CDI
+@Log4j2
+
+// Service: Asıl İş yükünü yapan yer demektir.
+@Service
+public class BlogCategoryServicesImpl implements IBlogCategoryServices<BlogCategoryDto, BlogCategoryEntity> {
+
+    // Field
+
+    // 1.YOL (Field Injection CDI)
+    /*@Autowired
+    private IBlogCategoryRepository iBlogCategoryRepository;*/
+
+    // 2.YOL (Constructor Injection CDI)
+    /*
+    private final IBlogCategoryRepository iBlogCategoryRepository;
+    @Autowired
+    public BlogCategoryServicesImpl(IBlogCategoryRepository iBlogCategoryRepository) {
+        this.iBlogCategoryRepository = iBlogCategoryRepository;
+    }
+     */
+
+    // 3.YOL (Field Injection CDI)
+    private final IBlogCategoryRepository iBlogCategoryRepository;
+    private final ModelMapperBean modelMapperBean;
+
+    // MODEL MAPPER SERVICE
+    @Override
+    public BlogCategoryDto entityToDto(BlogCategoryEntity entity) {
+        //1.YOL
+        //return modelMapperBean.modelMapperMethod().map(entity, BlogCategoryDto.class);
+
+        // 2.YOL
+        return BlogCategoryMapper.toDto(entity);
+    }
+
+    @Override
+    public BlogCategoryEntity dtoToEntity(BlogCategoryDto dto) {
+        return BlogCategoryMapper.toEntity(dto);
+    }
+
+
+    // ###############################################################
+    // SPEED DATA
+    @Override
+    public String categorySpeedData(Integer data) {
+        return "";
+    }
+
+
+    // ###############################################################
+    // CREATE (BLOG CATEGORY)
+    @Override
+    public BlogCategoryDto objectServiceCreate(BlogCategoryDto dto) {
+
+        // Hiç bir şey gelmiyorsa
+        if(dto==null || dto.getCategoryName()==null || dto.getCategoryName().isBlank() || dto.getCategoryName().isEmpty()) {
+            throw  new HamitMizrakException("Kategori adı zorunludur");
+        }
+
+        // Varsa
+        if(iBlogCategoryRepository.existsByCategoryNameIgnoreCase(dto.getCategoryName())) {
+            throw  new HamitMizrakException("Kategori zaten var: "+dto.getCategoryName());
+        }
+
+        // Entity
+        BlogCategoryEntity blogCategorySaved= iBlogCategoryRepository.save(dtoToEntity(dto));
+        return entityToDto(blogCategorySaved) ;
+    }
+
+    // LIST (BLOG CATEGORY)
+    @Override
+    public List<BlogCategoryDto> objectServiceList() {
+        return iBlogCategoryRepository.findAll().stream().map(this::entityToDto).toList();
+    }
+
+    // FIND BY ID (BLOG CATEGORY)
+    @Override
+    public BlogCategoryDto objectServiceFindById(Long id) {
+        BlogCategoryEntity blogCategoryEntity = iBlogCategoryRepository
+                .findById(id)
+                .orElseThrow(()-> new _404_NotFoundException(id+" nolu id bulunamadı"));
+        return entityToDto(blogCategoryEntity);
+    }
+
+    // UPDATE (BLOG CATEGORY)
+    @Override
+    public BlogCategoryDto objectServiceUpdate(Long id, BlogCategoryDto dto) {
+        return null;
+    }
+
+    // DELETE (BLOG CATEGORY)
+    @Override
+    public BlogCategoryDto objectServiceDelete(Long id) {
+        return null;
+    }
+
+
+}
