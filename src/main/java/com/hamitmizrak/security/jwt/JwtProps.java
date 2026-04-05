42396d8d549ca71106c3931dff3c3a60e0ed8ada
HamitMizrak <hamitmizrak@gmail.com>
2026-04-05 10:47:23 +0300

    JWT, Email JwtProps

--- src/main/java/com/hamitmizrak/security/jwt/JwtProps.java
+++ src/main/java/com/hamitmizrak/security/jwt/JwtProps.java
@@ -2,7 +2,6 @@ package com.hamitmizrak.security.jwt;
 
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
