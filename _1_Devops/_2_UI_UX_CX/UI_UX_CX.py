2e32888fdbb137813014655b97d3aa3204b7d71b
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 13:04:57 +0300

    Git; Config init clone status add log commit log diff show blame
    UI UX CX

---
+++ _1_Devops/_2_UI_UX_CX/UI_UX_CX.py
@@ -0,0 +1,42 @@
+"""
+S1-) UI,UX,CX Nedir ?
+C-1)
+- UI (User Interface): Kullanıcının sistemle temas ettiği görsel ve etkileşimsel yüzeyidir. Örneğin: Button, Form, Menü tipgrafi, renk, ikon, animasyon vb UI kapsamındadır.
+
+- UX (User eXprience): Kullanıcını bir hedef ulaşırken yaşadığı toplam deneyimdir. Sadece ekran güzelliği değildir.
+Örneğin: Hata önleme(Validation, hız hissi, güven, erişebilirlik, mennuniyet ve hedefe ulaşma kolaylığına denir.)
+
+- CX (Customer eXprience) : Ürün veya hizmetler tüm temas noktalarındaki toplam deneyimi ifade eder.
+Örneğin: Başvuru formu, çağrı merkezi, e-posta akışı, mobil uygulama
+
+ S2-) İyi bir UI/UX tasarımı ilkeleri nelerdir ?
+ - Tutarlılık
+ - Görsel hiyerarşisi
+ - Hata önleme
+ - Basitlik
+ - Erişebilirlik
+ - Esneklik
+
+ S3-) ISO 9241-11 Standart çerceve nedir ?
+ s3-) Kullanıbilirliği erkinlik, verimlikliktir.
+
+
+ S4-) Nielsen 10 kullanabilirlik sezgisi
+ C4-)
+- Sistem durumu görünür olmalı
+- Gerçek dünya ile uyumlu olmalı
+- Kullanıcı kontrol ve özgürlüğü sahip olmalı
+- Hata öneleme sağlanmalı
+- Esneklik ve verimlik sağlanmalı
+- Minimalist tasarım, estteki olmalı
+- Tutarlılık ve standartlar korunmalı
+- Hataları anlamlarının ve çözüm gösteren mesajlar sunmalı
+- Yardım ve dokümanları gerekirse ulaşılabilir olmalı
+-
+-
+
+
+
+
+
+"""
