3a79ea897e243fe6cc172d4e482565e152034b45
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 14:23:52 +0300

    Git; Config init clone status add log commit log diff show blame branch checkout switch --------
    UI UX CX
    -------
    
    Java Nedir
    JDK, JRE, JVM
     Java Mimarisi
    Access Modifier

--- _1_Devops/_3_Java/Deneme.java
+++
@@ -1,7 +0,0 @@
-package com.hamitmizrak.annotation;
-
-public class Deneme {
-    public static void main(String[] args) {
-        System.out.println("Deneme");
-    }
-}
--- src/main/java/com/hamitmizrak/annotation/Deneme.java
+++
@@ -1,4 +0,0 @@
-package com.hamitmizrak.annotation;
-
-public class Deneme {
-}
