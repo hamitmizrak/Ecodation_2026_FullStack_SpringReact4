dc1dfac377e5561b8ca52076e1e19fd9260286c4
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 13:31:51 +0300

    Git; Config init clone status add log commit log diff show blame
    UI UX CX
    
    Access Modifier

---
+++ _1_Devops/_3_Java/AccessModifier.py
@@ -0,0 +1,46 @@
+"""
+S1-) Access Modifier Nedir ?
+C1-) Bir sınıfın alanın, metodun, constructorun nereden erişilebilir olduğunu belirleyen anahtar kelimeleridr.
+-
+-
+
+
+S2-) Access Temel Amaçı Nedir ?
+C2-)
+- Kapsülleme (encapsulation)
+- Gereksiz bağımlılıkları azaltmak
+- Nesnenin iç durumu kontrolünü sağlamak
+-
+
+
+
+
+S3-) Access Modifier Çeşitleri Nedir ?
+C3-)
+
+Modifier   --- Aynı Sınıf ---Aynı Paket ---Alt Sınıf ---Farklı Paket      --Açıklama
+public           +              +           +             +                Heryerden erişimi vardır.
+protected        +              +           +             + (alt sınıf)    Kalıtım senaryolarında görünürlüğü sağlar
+private          +              -           -             -
+default          +              +           -             -
+
+
+S4-) Encapsulation İle Access Modifier İlişkisi Nedir ?
+C4-) Access modifer konusu OOP içindekiş encapsulation ilkesi ile doğrudan bağlantılıdır.
+- Nesnenin iç verisi mümkün olduğunca private tutulur.
+- Dışarıya yalnızca gerçekten ihtiyaç duyualn davranışı açılır Böylece veri bütünlüğü sağlanmış olunur.
+-
+-
+-
+
+
+S5-)
+C5-)
+-
+-
+-
+-
+-
+
+
+"""
