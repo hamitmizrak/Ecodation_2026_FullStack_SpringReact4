a42d1b16f7c8a715ed39541e2289cec5413e7c90
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 15:18:26 +0300

    Git; Config init clone status add log commit log diff show blame branch checkout switch --------
    UI UX CX
    -------
    
    Java Nedir
    JDK, JRE, JVM
     Java Mimarisi
    Access Modifier
    OOP

---
+++ _1_Devops/_3_Java/_3_OOP.py
@@ -0,0 +1,73 @@
+"""
+
+S-1) OOP nedir ?
+C-1) OOP(Object Orientend Programming)
+- Nesneye dayalı programlamad
+- Amaç: Gerçek dünya kavramlarını yazılıma daha yönetebilir tekrar kullanılabilir ve bakımı kolay şekilde yansıtmaktır.
+
+
+S-2) OOP'nin 4 temel İlkesi Nedir ?
+C-2) IPE-A
+- Inheritance     : Üst sınıftan özellik ve davranışı almak
+- Polymorphism    : Aynı referansttan farklı davranış üretir
+- Encapsulation  : Vei ve davranışı bir yapıda toplar
+- Abraction      : Gereksiz detayı gizler ve gerekli davranışı sınar
+
+
+S-3) Constructor, Overloading, Overriding nedir ?
+C-3)
+- Constructor : Nesne oluşturulduğunda ilk durumu belirler
+- Overloading : Aynı isimli metoun farklı parametrelerde tanımlanmasıdır
+- Overriding  : Alt sınıfın üst sınıftan gelen metodu kendi davranışına göre yeniden yazmasıdır. Örnek: Runtime polymorphism örneğidir
+
+
+S-4) Interface, abstract farkı
+C-4)
+- Interface : Davranış sözleşmesini tanımlar
+- Abstract  : Class ortak iskelet alanı + soyut davranış
+
+
+
+S-5) Projede Composition , Inheritance mi ?
+C-5)
+Composition
+- Güncel tasarım yaklaşımda çoğu durumda Composition tercih edilir
+- Composition  ==> Has an
+Örnek: Car has-an Enginee
+
+Inheritance
+- Kalıtım demek sıkı sıkıya bağlılık demektir.
+- Inheritance  ==> Is a
+Örnek: Dog is-a Animal
+
+
+S2-)
+C2-)
+-
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+-
+
+
+"""
