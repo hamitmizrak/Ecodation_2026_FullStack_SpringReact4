e728a40be8bbdbf47360d9da4301917ed8f2e2ad
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 14:12:11 +0300

    Git; Config init clone status add log commit log diff show blame branch checkout switch
    UI UX CX
    
    Java NEdir
    Access Modifier

---
+++ _1_Devops/_3_Java/_1_Java.py
@@ -0,0 +1,76 @@
+"""
+S1-) Java Nedir ?
+C1-) Java,
+-  Java 1995 yılında tanıtılan nesneye dayalı, paltformdan bağımsız çalışmayı hedefleyen kurumsal sistemlerde çok yagun kullanılan üst seviy4e bir dildir
+-
+-
+-
+
+
+S2-) Java Özellikleri Nelerdir ?
+C2-)
+- Nesneye dayalı
+-Platformdan bağımsız
+- Güvenli
+- Çok iş parçacığı bulunmaktadır.
+- Otomatik bellek yönetimi vardır.
+- Kurumsal uygunlu vardır (Spring, Jakarta EE, Hibernate)
+
+S2-) WORA nedir ?
+C2-)
+Write Once Run Anywhere (WORA):
+- Kaynak kod bir kez derlenir ve oluşan bytecode, uygun JVM olan farklı ortamlarda çalıaşbilir.
+-
+-
+
+S2-) Java'yı diğer programlama dillerinden ayıran özellikler nelerdir ?
+C2-) Java;
+- Java otomatik pointer yapar
+- JVM
+- Hem interpreter hemde compiler özelliği vardır
+- Kaynak kod bir kez derlenir(compiler) ve oluşan bytecode yorumlama(interpreter)
+
+S2-) JDK, JRE, JVM ve Java Mimarisi Neidr ?
+C2-) JDK> JRE > JVM
+- JVM(Java9 :
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+S2-)
+C2-)
+-
+-
+-
+
+
+"""
