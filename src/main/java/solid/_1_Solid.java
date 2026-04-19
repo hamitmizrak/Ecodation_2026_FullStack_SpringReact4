0220e267105e55229fe4e2d6ec002e77854144aa
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 15:51:00 +0300

    Git; Config init clone status add log commit log diff show blame branch checkout switch remote cherry-pick fetch pull push --------
    SOLID
    S
    --------
    
    UI UX CX
    -------
    
    Java Nedir
    JDK, JRE, JVM
     Java Mimarisi
    Access Modifier
    OOP

---
+++ src/main/java/solid/_1_Solid.java
@@ -0,0 +1,122 @@
+package solid;
+
+public class _1_Solid {
+
+    /*
+
+    S1-) SOLID Nedir ?
+    C1-) SOLID;
+     -Nesneye dayalı tasarım ,
+     - bakım
+     - genişletilmek
+     - test edilebilir olmasını hedefleyen beş temel prensip türüdür
+
+
+    S1-) SOLID Hakkında
+    C1-)
+     - Birbirinden bağımsız değildir
+     - Çoğu zaman beraber kullanılır
+     - Amaç sadece temiz kod yazmak değil dirençli mimari kurmaktır.
+     - Spring, Java EE, Jakarta EE SOLID çok sık kullanılır.
+
+
+    S1-) SOLID Açılımı Nedir ?
+    C1-) SOLID
+     - S: Single Responsibility
+     - O: Open/Closed
+     - L: Liskov Substitution
+     - I: Interface Segregation
+     - D: Dependency Inversion
+
+
+    S1-) SOLID Türkçe Anlamları ?
+    C1-)
+     - S: Single Responsibility  ==> Tek sorumluk
+     - O: Open/Closed            ==> Geneişlemeye açık
+     - L: Liskov Substitution    ==> Yerine geçebilirlik
+     - I: Interface Segregation  ==> Küçük ama amaç odaklı arayüz
+     - D: Dependency Inversion   ==> Soyuta bağlılık
+
+
+    S1-) S: Single Responsibility  ?
+    C1-) S:
+     - Anlamı: Bir sınıfın veya modülün yalnızca bir sorumluğu, dolayısıyla tek bir değişim nedeni olamalıdır.
+     - Amacı : Bakımı kolylaştırmak, yan etkileri azlatmak ve test kapsamını küçültmek
+     - Örneği: OrderService sadece sipariş kurallını yöneten: NotificationService
+     - İhlal belirtisi: Aynı sınıfta valdiation, veri tabanı kaydı, loglama , eposta gönderimi
+
+
+    S1-) O: Open/Closed  ?
+    C1-)
+     - Anlamı:
+     - Amacı :
+     - Örneği:
+     - İhlal belirtisi
+
+
+
+    S1-) L: Liskov Substitution  ?
+    C1-)
+     - Anlamı:
+     - Amacı :
+     - Örneği:
+     - İhlal belirtisi
+
+
+
+    S1-) I: Interface Segregation?
+    C1-)
+     - Anlamı:
+     - Amacı :
+     - Örneği:
+     - İhlal belirtisi
+
+
+
+    S1-) D: Dependency Inversion ?
+    C1-)
+     - Anlamı:
+     - Amacı :
+     - Örneği:
+     - İhlal belirtisi
+
+
+
+    S1-) SOLID Nedir ?
+    C1-)
+     -
+     -
+     -
+     -
+
+
+    S1-) SOLID Nedir ?
+    C1-)
+     -
+     -
+     -
+     -
+
+    S1-) SOLID Nedir ?
+    C1-)
+     -
+     -
+     -
+     -
+
+
+    S1-) SOLID Nedir ?
+    C1-)
+     -
+     -
+     -
+     -
+
+
+
+
+     */
+
+
+
+}
