f062359c8694c1d5f41bd7b99d9f9a34c72a859a
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-19 14:23:23 +0300

    Git; Config init clone status add log commit log diff show blame branch checkout switch --------
    UI UX CX
    -------
    
    Java Nedir
    JDK, JRE, JVM
     Java Mimarisi
    Access Modifier

--- _1_Devops/_3_Java/_1_Java.py
+++ _1_Devops/_3_Java/_1_Java.py
@@ -16,40 +16,56 @@ C2-)
 - Otomatik bellek yönetimi vardır.
 - Kurumsal uygunlu vardır (Spring, Jakarta EE, Hibernate)
 
-S2-) WORA nedir ?
-C2-)
+
+
+S3-) WORA nedir ?
+C3-)
 Write Once Run Anywhere (WORA):
 - Kaynak kod bir kez derlenir ve oluşan bytecode, uygun JVM olan farklı ortamlarda çalıaşbilir.
 -
 -
 
-S2-) Java'yı diğer programlama dillerinden ayıran özellikler nelerdir ?
-C2-) Java;
+
+
+S4-) Java'yı diğer programlama dillerinden ayıran özellikler nelerdir ?
+C4-) Java;
 - Java otomatik pointer yapar
 - JVM
 - Hem interpreter hemde compiler özelliği vardır
 - Kaynak kod bir kez derlenir(compiler) ve oluşan bytecode yorumlama(interpreter)
 
-S2-) JDK, JRE, JVM ve Java Mimarisi Neidr ?
-C2-) JDK> JRE > JVM
-- JVM(Java9 :
+
+
+S5-) JDK, JRE, JVM bunları açıklayınız ?
+C5-) JDK> JRE > JVM
+
+- JVM (Java Virtula Machine) : Bytecode'u okuyan ve çalıştıran sanal makinedir.
+- JVM : Class loading, verification, execution, Garbarage collection görevlerini yapar
+
+- JRE (Java Run time environment): Java uygulamasını çalıştırmak için runtime paketidir.
+- JRE: JVM + Temel kütüphaneleri burada yer alır
+
+- JDK (Java Virtual Machine): Java geliştirmek için gerekn araçlardır.
+- JDK : javac derleyicisi, debugger, javadoc ve diğer geliştirme araçları mevcuttur.
+
+
+
+S6-) Java Mimarisi Açıklayınız ?
+C6-)
+- Deneme.java (Kaynak Kodu)  ==> Compiler (Javac) ==> Bytecode (Deneme.class) ==>
+  ==> JRE içindeki JVM ==> Class Loader+ Byte Verifier +Execution Engine +Garbarage Collection
+
 -
 -
 
-S2-)
-C2-)
+S7-) Compiler, Interpreter, JIT Nedir ve ilişkileri
+C7-)
 -
 -
 -
 
-S2-)
-C2-)
--
--
--
-
-S2-)
-C2-)
+S8-) Java Uygualamasını Yaşam döngüsü ?
+C8-)
 -
 -
 -
